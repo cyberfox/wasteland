25a726bb8a5ea7797280b5361c43ee59156d2805
Morgan Schweers <cyberfox@gmail.com>
2009-03-09 02:23:50 -0700

    Change out the array structure for a hand-rolled pair structure, in case it was array handling that was borked.  (After all it always blew up on an NSArrayReference or something like that.  Turns out it still blows up, now on an ivar_get method. :(

--- WordOutline.rb
+++ WordOutline.rb
@@ -7,6 +7,14 @@
 #
 
 class WordOutline
+  class Pair
+    attr_reader :key, :value
+    def initialize(first, second)
+      @key = first
+      @value = second
+    end
+  end
+
   def initialize(words, word_hash)
     @words = words
     @depth = deep_map(word_hash)
@@ -14,35 +22,41 @@ class WordOutline
     @hash = word_hash
   end
 
-  # Possible items...  nil, [key, {hash}], [key, string]
+  # Possible items...  nil, Pair.new(key, {hash}), Pair.new(key, string)
   def outlineView(view, numberOfChildrenOfItem:item)
-    puts "nOCOI: #{item.inspect}"
+#    puts "nOCOI: #{item.inspect}"
     return @hash.length if item.nil?
-    return (item.last.is_a?(String) ? 1 : item.last.length) if item.is_a?(Array)
+    if item.is_a?(Pair)
+      last = item.value
+      return 1 if last.is_a?(String)
+      return last.length
+    end
     return 0
   end
 
   def outlineView(view, isItemExpandable:item)
-    return !item.is_a?(String)
+    return item.is_a?(Pair)
   end
 
   # Return the actual child, not the child that will be used for display.
   def outlineView(view, child:index, ofItem:item)
-    puts "child: #{index.inspect}, #{item.inspect}"
-    return [@hash.keys[index], @depth[@hash.keys[index]]] if item.nil?
-    if(item.is_a?(Array))
-      return item.last if item.last.is_a?(String)
-      key = item.last.keys[index]
-      return [key, item.last[key]]
+#    puts "child: #{index.inspect}, #{item.inspect}"
+    return Pair.new(@hash.keys[index], @depth[@hash.keys[index]]) if item.nil?
+    if(item.is_a?(Pair))
+      return item.value if item.value.is_a?(String)
+      key = item.value.keys[index]
+      return Pair.new(key, item.value[key])
     end
     return nil
   end
 
   def outlineView(view, objectValueForTableColumn:tableColumn, byItem:item)
-    puts "oVFTC: #{tableColumn.inspect}, #{item.inspect}, #{item.class}"
-    item = item.first if !item.is_a?(String) && item.respond_to?(:first) && item.length > 0
-    result = item.to_s
-    puts "oVFTC returning: #{result.inspect}"
+#    puts "oVFTC: #{tableColumn.inspect}, #{item.inspect}, #{item.class}"
+    result = item
+    puts __LINE__
+    result = item.key if item.is_a?(Pair)
+    puts __LINE__
+#    puts "oVFTC returning: #{result.inspect}"
     return result
   end
 
